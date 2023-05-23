59bc6bd217d437cca95eb062013826d12f5af1c8
Katerine Witkoski <ka.lwitkoski@gmail.com>
2023-05-23 09:15:26 -0300

    adiciona classe Usuario

--- src/main/java/prototipodb/model/Usuario.java
+++ src/main/java/prototipodb/model/Usuario.java
@@ -1,4 +1,18 @@
 package prototipodb.model;
-
+/*
+Atributos:
+- Nome (String)
+- RA (long)
+ */
 public class Usuario {
+    public String nome;
+    public long RA;
+
+    // Método construtor:
+    public Usuario(String nome, long RA){
+        this.nome = nome;
+        this.RA = RA;
+    }
+
+
 }
