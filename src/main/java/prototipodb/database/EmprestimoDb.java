20f53cbffc2bf83e72f976dfb64d530e63a5128c
TaylorNathalie <nathalietaylortech@gmail.com>
2023-05-30 16:00:00 -0300

    classe e metodos Emprestimo

--- src/main/java/prototipodb/database/EmprestimoDb.java
+++ src/main/java/prototipodb/database/EmprestimoDb.java
@@ -1,11 +1,15 @@
 package prototipodb.database;
+import prototipodb.model.Categoria;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.*;
+
 /*
 Atributos:
 - Codigo Livros (Chave estrangeira da tabela livros)
-- Nome (String)
-- Autor (String)
 - Status (String - somente 3 possíveis - Reservado/Excluído/Disponível)
-- Nome Leitor (recebido da tabela Usuário - será que é melhor passar informação em forma de número de código?)
+- RA_usuario (String)
 
 Métodos:
 - Criar (CREATE) -
@@ -14,4 +18,72 @@ Métodos:
  */
 
 public class EmprestimoDb {
+     private Database database;
+     
+     // Método construtor:
+    public EmprestimoDb(Database database){
+        this.database = database;
+    }
+    
+    public void criarStatus(String novoStatus) throws Exception {
+        String sql = "INSERT INTO emprestimo (status_livro) VALUES (?)";
+        PreparedStatement instrucao = this.database.getConnection().prepareStatement(sql);
+        
+        // Definir os valores dos parâmetros
+        instrucao.setString(1, novoStatus);
+
+        // Executar a instrução SQL da variável `instrucao`
+        instrucao.executeUpdate();
+
+        System.out.println("Status criado!");
+
+    }
+
+    public void alterarStatus(String novoStatus, int codigo, String RA) throws Exception {
+        String sql = "UPDATE emprestimo SET status_livro = (?) WHERE cod_livro = (?) && RA_ = (?)";
+        PreparedStatement instrucao = this.database.getConnection().prepareStatement(sql);
+
+        // Definir os valores dos parâmetros
+        instrucao.setString(1, novoStatus);
+        instrucao.setInt(2, codigo);
+        instrucao.setString(3, RA);
+
+        // Executar a instrução SQL da variável `instrucao`
+        instrucao.executeUpdate();
+
+
+        System.out.println("Status alterado!");
+    }
+
+    public Emprestimo[] lerEmprestimo() throws Exception {
+        // Preciso definir o tamanho do vetor
+        String sqlCountEmprestimos = "SELECT count(*) FROM emprestimo";
+        PreparedStatement instrucaoCountEmprestimos = this.database.getConnection().prepareStatement(sqlCountEmprestimos);
+        ResultSet resultadosCountEmprestimos = instrucaoCountEmprestimos.executeQuery(sqlCountEmprestimos);
+        resultadosCountEmprestimos.next();
+        int maximoNumeroDeResultados = resultadosCountEmprestimos.getInt(1);
+
+        String sql = "SELECT * FROM emprestimo LIMIT " +  maximoNumeroDeResultados;
+        PreparedStatement instrucao = this.database.getConnection().prepareStatement(sql);
+
+
+        // Executar a instrução SQL da variável `instrucao`
+        ResultSet resultados = instrucao.executeQuery(sql);
+        Emprestimo[] emprestimos = new Emprestimo[maximoNumeroDeResultados];
+
+        int i = 0;
+        while (resultados.next()) { 
+            String statusLivro = resultados.getString(2);
+            int codigoLivro = resultados.getInt(1);
+            String raLeitor = resultados.getString(2);
+            
+            Emprestimo Emprestimo = new Emprestimo (statusLivro, codigoLivro, raLeitor);
+            Emprestimos[i] = emprestimo;
+            i = i + 1;
+        }
+
+        return emprestimo;
+    }
+
+    
 }
