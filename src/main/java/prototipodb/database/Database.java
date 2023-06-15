06c1600518a7bdac2646af9f62e7c6b1bd68fb29
Katerine Witkoski <ka.lwitkoski@gmail.com>
2023-06-15 17:00:06 -0300

    inclue nomes dos participantes

--- src/main/java/prototipodb/database/Database.java
+++ src/main/java/prototipodb/database/Database.java
@@ -10,7 +10,7 @@ public class Database {
     //Método Construtor da Classe
     public Database(String serverName, String databaseName, String password, String username) throws Exception {
         /* EXCEPTION é a classe do java que simboliza erros
-        - esse throws Eception está dizendo que esse método pode levantar uma exceção (um erro)
+        - esse throws Exception está dizendo que esse método pode levantar uma exceção (um erro)
         - throw é jogar
         - "try/catch - tentar/pegar"
         - o throw define na assinatura do método que ele estourar? (dar erro, jogar uma exceção)
